d6c6214ae4ba6bf71a4779f4c0135169affebbee
jackalstv <andredor700@gmail.com>
2024-02-18 12:20:17 +0100

    BOUTONPRESS SETUP

--- src/Portaudio.c
+++ src/Portaudio.c
@@ -17,7 +17,7 @@ void* keyboardInput(void* arg) {
     while (1) {
         if(scanf("%99s", input)){
             boutonPress=1;
-        }else if(scanf("%99s", input) && boutonPress==1)
+        }else{
         boutonPress=0;
 
     }
@@ -46,7 +46,7 @@ int paCallback(const void *inputBuffer, void *outputBuffer,
             }
             if(boutonPress==1){
                 isPress[i]=-1;
-            }else{
+            }else if(boutonPress=0 && isPress[i]==-1){
                 isPress[i]=0;
             }
             printf("%i\n",isPress[i]);
