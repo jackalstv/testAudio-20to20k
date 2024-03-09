35240dab1dd0b6ce987c0d348d7c15c0904728c6
jackalstv <andredor700@gmail.com>
2024-03-09 14:55:24 +0100

    traitement.c plus des optimisation

--- src/traitement.c
+++ src/traitement.c
@@ -6,46 +6,22 @@
 
 void traitementGraph(int * data){
 
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        if(i<ARRAY_SIZE-13){
+    int v;
 
-            if(data[i]==-1 && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==0 && data[i+6]==0 && data[i+7]==0 && data[i+8]==0 && data[i+9]==0 && data[i+10]==0 && data[i+11]==0 && data[i+12]==-1){
-                data[i+1]=-1;
-                data[i+2]=-1;
-                data[i+3]=-1;
-                data[i+4]=-1;
-                data[i+5]=-1;
-                data[i+6]=-1;
-                data[i+7]=-1;
-                data[i+8]=-1;
-                data[i+9]=-1;
-                data[i+10]=-1;
-                data[i+11]=-1;
-                i=i+12;
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+        if(data[i]==-1) {
+            for (int j = i; j < ARRAY_SIZE; j++) {
+                if (data[j] == -1 && (j - i) > 14) {
+                    v = j;
+                    break;
+                }
+            }
+            for (int j = 1; j < v; j++) {
+                if (data[j] == 0) {
+                    data[j] = -1;
+                }
             }
         }
-        if(i<ARRAY_SIZE-9){
-            if(data[i]==-1 && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==0 && data[i+6]==0 && data[i+7]==0 && data[i+8]==0 && data[i+9]==-1){
-                data[i+1]=-1;
-                data[i+2]=-1;
-                data[i+3]=-1;
-                data[i+4]=-1;
-                data[i+5]=-1;
-                data[i+6]=-1;
-                data[i+7]=-1;
-                data[i+8]=-1;
-                i=i+9;
-            }
-        }
-        if(i<ARRAY_SIZE-5){
-            if(data[i]==-1 && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==-1){
-                data[i+1]=-1;
-                data[i+2]=-1;
-                data[i+3]=-1;
-                data[i+4]=-1;
-                i=i+5;
-            }
-        }
-        printf("Donnee formater \n");
-        }
+    }
+    printf("Donnee formater \n");
 }
