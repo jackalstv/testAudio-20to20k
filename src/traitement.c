73921f92ce013b808058ca35ef75c33133e6b09f
jackalstv <andredor700@gmail.com>
2024-03-09 15:22:59 +0100

    traitement.c plus des optimisation

--- src/traitement.c
+++ src/traitement.c
@@ -9,16 +9,26 @@ void traitementGraph(int * data){
     int v;
 
     for(int i = 0; i < ARRAY_SIZE; i++) {
-        if(data[i]==-1) {
+        if((data[i]==-1 || data[i]==-2 || data[i]==-3) && i<ARRAY_SIZE-13){
             for (int j = i; j < 14; j++) {
-                if (data[j] == -1){
+                if (data[j]==-1 || data[j]==-2 || data[j]==-3){
                     v = j;
                     break;
                 }
             }
             for (int w=i;w < v;w++) {
-                if (data[w] == 0) {
-                    data[w] = -1;
+                switch (data[w]) {
+                    case -1:
+                        data[w] = -1;
+                        break;
+                    case -2:
+                        data[w] = -2;
+                        break;
+                    case -3:
+                        data[w] = -3;
+                        break;
+                    default:
+                        break;
                 }
             }
         }
