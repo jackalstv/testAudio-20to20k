8dbfed03d3068f871bcd63d3a6c320cb3c152530
jackalstv <andredor700@gmail.com>
2024-03-09 19:03:58 +0100

    traitement.c plus des optimisation

--- src/traitement.c
+++ src/traitement.c
@@ -5,42 +5,23 @@
 #include "../include/traitement.h"
 
 void traitementGraph(int * data){
-
+    int v;
     for(int i = 0; i < ARRAY_SIZE-15; i++) {
         for(int w= -1; w > -4; w--){
-            if(data[i]==w && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==0 && data[i+6]==0 && data[i+7]==0 && data[i+8]==0 && data[i+9]==0 && data[i+10]==0 && data[i+11]==0 && data[i+12]==w){
-                for(int j = i; j < 13; j++){
-                    data[i+j]=w;
+            for(int u = 0; u < 13; u++) {
+                if (data[i] == w) {
+                    for (int j = i + 1; j < 13; j++) {
+                        if (data[j] == w) {
+                            v = j;
+                            for (int o = i; o < j; o++) {
+                                data[o] = w;
+                            }
+                        }
+                    }
                 }
-                i=i+12;
             }
-            if(data[i]==w && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==0 && data[i+6]==0 && data[i+7]==0 && data[i+8]==0 && data[i+9]==0 && data[i+10]==0 && data[i+11]==w){
-                for(int j = i; j < 12; j++){
-                    data[i+j]=w;
-                }
-                i=i+11;
-            }
-            if(data[i]==w && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==0 && data[i+6]==0 && data[i+7]==0 && data[i+8]==0 && data[i+9]==0 && data[i+10]==w){
-                for(int j = i; j < 11; j++){
-                    data[i+j]=w;
-                }
-                i=i+10;
-            }
-            if(data[i]==w && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==0 && data[i+6]==0 && data[i+7]==0 && data[i+8]==0 && data[i+9]==w){
-                for(int j = i; j < 10; j++){
-                    data[i+j]=w;
-                }
-                i=i+9;
-                }
-            if(data[i]==-1 && data[i+1]==0 && data[i+2]==0 && data[i+3]==0 && data[i+4]==0 && data[i+5]==-1){
-                data[i+1]=-1;
-                data[i+2]=-1;
-                data[i+3]=-1;
-                data[i+4]=-1;
-                i=i+5;
-            }
-    }
-
         }
+        i=v;
+    }
     printf("Donnee formater \n");
 }
