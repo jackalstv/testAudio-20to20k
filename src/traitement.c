d8e04c6a6a394761e43d1e826a737a32faf0fa92
jackalstv <andredor700@gmail.com>
2024-03-09 19:30:52 +0100

    traitement.c plus des optimisation

--- src/traitement.c
+++ src/traitement.c
@@ -4,24 +4,26 @@
 
 #include "../include/traitement.h"
 
-void traitementGraph(int * data){
-    int v;
-    for(int i = 0; i < ARRAY_SIZE-15; i++) {
-        for(int w= -1; w > -4; w--){
-            for(int u = 0; u < 13; u++) {
+void traitementGraph(int *data) {
+    int v = 0; // Initialisez v pour éviter un comportement indéterminé
+    for (int i = 0; i < ARRAY_SIZE - 15; i++) {
+        for (int w = -1; w > -4; w--) {
+            for (int u = 0; u < 13; u++) {
                 if (data[i] == w) {
-                    for (int j = i + 1; j < 13; j++) {
+                    for (int j = i + 1; j < ARRAY_SIZE; j++) { // Corrigez la limite de tableau
                         if (data[j] == w) {
                             v = j;
                             for (int o = i; o < j; o++) {
                                 data[o] = w;
                             }
+                            break; // Quittez la boucle une fois que la valeur est trouvée
                         }
                     }
                 }
             }
         }
-        i=v;
+        i = v; // Utilisez v après avoir terminé la boucle interne
     }
     printf("Donnee formater \n");
 }
+
