75a967a1da4cbe30705a19e151302a9dbe34b3ca
jackalstv <andredor700@gmail.com>
2024-03-11 20:02:41 +0100

    ajustement audio gramme

--- src/graphe.c
+++ src/graphe.c
@@ -5,21 +5,21 @@
 #include "../include/graphe.h"
 
 void generate_graph(int *data) {
-    FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
-    if (gnuplotPipe == NULL) {
+    FILE *gnuplotPipe = popen("gnuplot -persistent", "w"); // Ouverture du pipe vers Gnuplot
+    if (gnuplotPipe == NULL) { // Si l'ouverture du pipe a échoué
         printf("Erreur lors de l'ouverture du pipe vers Gnuplot.\n");
         exit(1);
     }
 
-    fprintf(gnuplotPipe, "set title 'Audiogram de 20hz à 20Khz'\n");
-    fprintf(gnuplotPipe, "plot '-' with linespoints\n");
+    fprintf(gnuplotPipe, "set title 'Détection de weqewqeqweqwewqewq de touche'\n"); // Titre du graphique
+    fprintf(gnuplotPipe, "plot '-' with linespoints\n"); // Commande pour tracer le graphique
 
-    for(int i = 0; i < ARRAY_SIZE+1; i++) { // Parcours du tableau
+    for(int i = 0; i < ARRAY_SIZE; i++) { // Parcours du tableau
         fprintf(gnuplotPipe, "%d %d\n", i, data[i]); // Écriture des données dans le pipe
     }
 
     fprintf(gnuplotPipe, "e\n"); // Fin de l'écriture des données
-    fflush(gnuplotPipe);
+    fflush(gnuplotPipe); // Vidage du buffer
 
-    pclose(gnuplotPipe);
+    pclose(gnuplotPipe); // Fermeture du pipe
 }
