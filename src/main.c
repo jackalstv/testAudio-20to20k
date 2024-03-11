c3baea2ccdbc40dd0420c1bb222f15dd8e63fb03
jackalstv <andredor700@gmail.com>
2024-03-11 19:54:55 +0100

    ajustement audio gramme

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <portaudio.h>
+#include <math.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <pthread.h>
 #include "../include/Portaudio.h"
 #include "../include/graphe.h"
+#include "../include/global.h"
+#include "../include/kaybordact.h"
 #include "../include/traitement.h"
 
 bool record = true;
@@ -16,8 +19,10 @@ volatile int keyPressed=0;
 
 int main(void) {
     FILE *gnuplot = popen("gnuplot", "w");
+    //volatile int keyPressed = 0; // Définition de la variable globale
     PaError err;
     PaStream *stream;
+    //int size = (int)END_FREQ- (int)START_FREQ + 1;
     int numSamples = (int)(DURATION * SAMPLING_RATE);
     double *signal = (double *)malloc(numSamples * sizeof(double));
 
